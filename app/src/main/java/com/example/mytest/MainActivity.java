2f1b650a5b8e24412bf61d782bc666a071dcbb6e
lucky6111 <hsueh.hey2@gmail.com>
2020-03-12 08:45:01 +0800

    ver3

--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -16,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("sam","test");
         Log.d("sam","test");
         Log.d("sam","test");
+
+        Log.d("sam","test");
     }
 }
